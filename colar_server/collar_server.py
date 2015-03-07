7018679e77811b2e38e8c43cbf176ac4cfa51670
javl <mail@jaspervanloenen.com>
2015-03-07 15:25:51 +0100

    Added server file

---
+++ colar_server/collar_server.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+import os, sys, serial, commands, time
+
+from flask import Flask
+app = Flask(__name__)
+app.debug  = True
+ser = None
+
+@app.route('/cmd/<cmd_id>/')
+def page(cmd_id):
+	if cmd_id == "0":
+		return "Ignore zero"
+	else:
+		print "Send out number %s" % str(cmd_id)
+		ser.write("<%s>" % str(cmd_id))
+		return "Cmd %s sent" % cmd_id
+
+def main():
+	global ser
+	try:
+		# Find Arduino on Linux
+		status, address = commands.getstatusoutput('ls /dev | grep tty.usbmodem')
+		if address == "":
+			# Find Arduino on Windows
+			status, address = commands.getstatusoutput('ls /dev | grep ttyUSB')
+			if address == "":
+		 		exit("No Arduino found...")
+		ser = serial.Serial("/dev/"+address, 4800)
+		print ser
+		app.run(host='0.0.0.0', port=8080, use_reloader=False)
+
+	except KeyboardInterrupt:
+		ser.close()
+	except Exception:
+		ser.close()
+		sys.exit(0)
+
+if __name__ == "__main__":
+	main()
